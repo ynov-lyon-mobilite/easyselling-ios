adb32afa0b75d7b3f0994b1de04b8a87e10e7dd0
MaxenceMottard <maxence.mottard@worldline.com>
2021-10-06 11:03:32 +0200

    setup github actions

--- easysellingTests/easysellingTests.swift
+++ easysellingTests/easysellingTests.swift
@@ -8,7 +8,7 @@
 import XCTest
 @testable import easyselling
 
-class easysellingTests: XCTestCase {
+class EasysellingTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -29,5 +29,4 @@ class easysellingTests: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
-
 }
