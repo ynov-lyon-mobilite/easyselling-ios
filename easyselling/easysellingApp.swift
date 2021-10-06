9cd77c4801860f5d9c7f82ef1d5d4c21ea9f35a5
MaxenceMottard <maxence.mottard@worldline.com>
2021-10-06 09:15:09 +0200

    Initial Commit

---
+++ easyselling/easysellingApp.swift
@@ -0,0 +1,17 @@
+//
+//  easysellingApp.swift
+//  easyselling
+//
+//  Created by Maxence on 06/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct easysellingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
