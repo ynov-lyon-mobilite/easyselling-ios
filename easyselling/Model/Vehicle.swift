eff5bd45ff055630488e806ead9be5a551d4d5fa
TANCHOUX Théo <tanchoux71@gmail.com>
2021-10-13 17:28:39 +0200

    [WIP] Home view, VehicleService

--- easyselling/Model/Vehicle.swift
+++ easyselling/Model/Vehicle.swift
@@ -7,17 +7,35 @@
 
 import Foundation
 
-struct Vehicle: Decodable {
+struct Vehicle: Decodable, Identifiable {
     let id: String
     let owner: String
     let license: String
     let model: String
     let brand: String
-    let type: String
-    let category: String?
+    let type: CarType
+    let category: CarCategory?
+    let year: String
     let userAccess: [String]
     
     enum CodingKeys: String, CodingKey {
-        case id, owner, license, model, brand, type, category, userAccess = "user_access"
+        case id, owner, license, model, brand, type, category, year, userAccess = "user_access"
     }
+}
+
+enum CarType: String, Decodable {
+    case car, moto
+    
+    var description: String {
+        switch self {
+        case .car:
+            return "Voiture"
+        case .moto:
+            return "Moto"
+        }
+    }
+}
+
+enum CarCategory: String, Decodable {
+    case mtt1, mtt2
 }
