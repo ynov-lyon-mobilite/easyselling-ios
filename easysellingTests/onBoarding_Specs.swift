d741dde05e5b19c64bf20270390081d83feee678
pierre <Pierre_gourgouillon@hotmail.com>
2021-10-13 12:18:10 +0200

    create test file

---
+++ easysellingTests/onBoarding_Specs.swift
@@ -0,0 +1,13 @@
+//
+//  onBoarding_Specs.swift
+//  easysellingTests
+//
+//  Created by Pierre on 13/10/2021.
+//
+
+import XCTest
+@testable import easyselling
+
+class OnBoardingTests: XCTestCase{
+    
+}
