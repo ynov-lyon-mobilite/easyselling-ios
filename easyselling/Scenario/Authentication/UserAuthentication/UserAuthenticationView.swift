a5682d472ed9326ab167de216d91b6d8e45c2abe
Maxence <mottard.maxence@gmail.com>
2021-11-02 05:04:11 +0100

    Feature/login view (#11)
    * implement view model in view
    
    Test & Create DefaultUserAuthenticator
    
    remove file which failed ci
    
    implement view model in view
    
    * remove useless logs
    
    * Create scenario to navigate between userAuthentication & accountCreation
    
    * Move files
    
    * test account creation opening
    
    * rename account navigator
    
    * fix tests
    
    * Create logic for view model errors
    
    * show errors in alert view
    
    * fixs after review
    
    Co-authored-by: MaxenceMottard <maxence.mottard@worldline.com>

---
+++ easyselling/Scenario/Authentication/UserAuthentication/UserAuthenticationView.swift
@@ -0,0 +1,65 @@
+//
+//  UserAuthenticationView.swift
+//  easyselling
+//
+//  Created by Maxence on 27/10/2021.
+//
+
+import SwiftUI
+
+struct UserAuthenticationView: View {
+    @ObservedObject private var viewModel: UserAuthenticationViewModel
+    
+    init(viewModel: UserAuthenticationViewModel) {
+        self.viewModel = viewModel
+    }
+    
+    var body: some View {
+        VStack(spacing: 30) {
+            Spacer()
+            
+            VStack {
+                TextField(L10n.SignUp.mail, text: $viewModel.email)
+                    .padding(6)
+                    .background(Color.gray.opacity(0.5))
+                    .cornerRadius(10)
+                    .textContentType(.emailAddress)
+
+                SecureField(L10n.SignUp.password, text: $viewModel.password)
+                    .padding(6)
+                    .background(Color.gray.opacity(0.5))
+                    .cornerRadius(10)
+                    .textContentType(.password)
+            }
+            
+            Button(L10n.UserAuthentication.Button.login) {
+                Task {
+                    await viewModel.login()
+                }
+            }
+            .foregroundColor(Color.white)
+            .padding()
+            .background(Color.black)
+            .cornerRadius(30)
+            
+            Spacer()
+            
+            Button(L10n.UserAuthentication.Button.register) {
+                viewModel.navigateToAccountCreation()
+            }
+        }
+        .padding(.horizontal, 50)
+        .alert(isPresented: $viewModel.hasError) {
+            Alert(
+                title: Text(viewModel.error?.errorDescription ?? ""),
+                dismissButton: Alert.Button.default(Text("Ok")))
+        }
+    }
+}
+
+struct UserAuthenticationView_Previews: PreviewProvider {
+    static var previews: some View {
+        let viewModel = UserAuthenticationViewModel(navigateToAccountCreation: {})
+        UserAuthenticationView(viewModel: viewModel)
+    }
+}
---
+++ easyselling/Scenario/Authentication/UserAuthentication/UserAuthenticationViewModel.swift
@@ -0,0 +1,71 @@
+//
+//  UserAuthenticationViewModel.swift
+//  easyselling
+//
+//  Created by Maxence on 20/10/2021.
+//
+
+import Foundation
+
+final class UserAuthenticationViewModel: ObservableObject {
+    private let tokenManager: TokenManager
+    private let userAuthenticator: UserAuthenticatior
+    
+    let navigateToAccountCreation: Action
+
+    @Published var email: String = ""
+    @Published var password: String = ""
+    
+    @Published var hasError = false
+    @Published var error: ViewModelError? {
+        didSet {
+            hasError = (error != nil)
+        }
+    }
+    
+    init(navigateToAccountCreation: @escaping Action, userAuthenticator: UserAuthenticatior = DefaultUserAuthenticator(), tokenManager: TokenManager = .shared) {
+        self.userAuthenticator = userAuthenticator
+        self.tokenManager = tokenManager
+        self.navigateToAccountCreation = navigateToAccountCreation
+    }
+    
+    func verifyInformations() throws {
+        if email.isEmpty { throw ViewModelError.emptyEmail }
+        if password.isEmpty { throw ViewModelError.emptyPassword }
+    }
+    
+    @MainActor func login() async {
+        do {
+            try verifyInformations()
+            let token = try await userAuthenticator.login(mail: email, password: password)
+            tokenManager.accessToken = token.accessToken
+            tokenManager.refreshToken = token.refreshToken
+        } catch(let error) {
+            if let error = error as? ViewModelError {
+                self.error = error
+            } else if (error as? APICallerError) == .unauthorized {
+                self.error = .badCredentials
+            } else {
+                self.error = .unknow
+            }
+        }
+    }
+    
+    enum ViewModelError: LocalizedError, Equatable {
+        case emptyEmail
+        case emptyPassword
+        case badCredentials
+        case unknow
+        
+        var isAlertError: Bool { [.badCredentials, .unknow].contains(self) }
+
+        var errorDescription: String? {
+            switch self {
+            case .emptyEmail: return "L'addresse email est vide"
+            case .emptyPassword: return "Le mot de passe est vide"
+            case .badCredentials: return "Le couple identifiant/mot de passe est incorrect. Veuillez réessayer."
+            case .unknow: return "Une erreur est survenue"
+            }
+        }
+    }
+}
