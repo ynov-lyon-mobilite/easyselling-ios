120076f404562f94a0220bf59ad2f9007c3a3518
MaxenceMottard <maxence.mottard@worldline.com>
2021-11-09 06:23:54 +0100

    upload file request
    # Conflicts:
    #	easyselling.xcodeproj/project.pbxproj

---
+++ easyselling/Services/DefaultFileUploader.swift
@@ -0,0 +1,50 @@
+//
+//  DefaultFileUploader.swift
+//  easyselling
+//
+//  Created by Maxence on 09/11/2021.
+//
+
+import Foundation
+
+protocol FileUploader {
+    func upload(filename: String, filetype: String, data: Data) async throws -> UploadedFile
+}
+
+final class DefaultFileUploader: FileUploader {
+    private var requestGenerator: RequestGenerator
+    private var apiCaller: APICaller
+    
+    init(requestGenerator: RequestGenerator = DefaultRequestGenerator(), urlSession: URLSessionProtocol = URLSession.shared) {
+        self.requestGenerator = requestGenerator
+        self.apiCaller = DefaultAPICaller(urlSession: urlSession)
+    }
+    
+    func upload(filename: String, filetype: String, data: Data) async throws -> UploadedFile {
+        let boundaryConstant = UUID().uuidString
+        let contentType = "multipart/form-data; boundary=" + boundaryConstant
+        
+        guard let boundaryStart = "--\(boundaryConstant)\r\n".data(using: .utf8),
+              let boundaryEnd = "--\(boundaryConstant)--\r\n".data(using: .utf8),
+              let contentDispositionString = "Content-Disposition: form-data; name=\"file\"; filename=\"\(filename)\"\r\n".data(using: .utf8),
+              let contentTypeString = "Content-Type: \(filetype)\r\n\r\n".data(using: .utf8),
+              let separator = "\r\n".data(using: .utf8) else {
+                  throw APICallerError.requestGenerationError
+              }
+
+        var body = Data()
+        body.append(boundaryStart)
+        body.append(contentDispositionString)
+        body.append(contentTypeString)
+        body.append(data)
+        body.append(separator)
+        body.append(boundaryEnd)
+        
+        var urlRequest = try requestGenerator.generateRequest(endpoint: .files, method: .POST, headers: [
+            "Content-Type": contentType
+        ])
+        urlRequest.httpBody = body
+        
+        return try await apiCaller.call(urlRequest, decodeType: UploadedFile.self)
+    }
+}
