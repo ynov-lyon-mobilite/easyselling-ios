6d485790c7a3600b7d6e7d5ba4052cd4f12fd989
CorentinLrcn <corentin.laurencine@ynov.com>
2021-11-24 07:28:53 +0100

    Solving rebase conflicts

---
+++ easysellingTests/Invoice/InvoiceScenario_Specs.swift
@@ -0,0 +1,17 @@
+//
+//  InvoiceScenario_Specs.swift
+//  easysellingTests
+//
+//  Created by Corentin Laurencine on 11/11/2021.
+//
+
+import Foundation
+import XCTest
+
+class InvoiceScenario_Spec: XCTestCase {
+    
+    func test_Gets_invoices_of_a_vehicule() {
+        
+    }
+    
+}
