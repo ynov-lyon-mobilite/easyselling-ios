78521a382c095c288ed19840359f7b2afcc7a033
CorentinLrcn <corentin.laurencine@ynov.com>
2021-11-24 09:38:51 +0100

    debut test obtention facture

--- easysellingTests/Invoice/InvoiceScenario_Specs.swift
+++ easysellingTests/Invoice/InvoiceScenario_Specs.swift
@@ -9,9 +9,53 @@ import Foundation
 import XCTest
 
 class InvoiceScenario_Spec: XCTestCase {
+    private var result: Bool!
+    private let idVehicule: String = "Dej-123-dfghjd-45-ajhdk-6789"
     
-    func test_Gets_invoices_of_a_vehicule() {
+    func test_Opens_invoice_screen() {
         
     }
     
+    func test_Succeeds_getting_invoices_of_a_vehicule() {
+        let invoiceService = InvoiceService(httpCode: 200)
+        invoiceService.getInvoice(idVehicule: idVehicule) { success in
+            self.result = success
+        }
+        XCTAssertTrue(self.result)
+    }
+    
+    func test_Fails_getting_invoices_of_a_vehicule() {        
+        let invoiceService = InvoiceService(httpCode: 400)
+        invoiceService.getInvoice(idVehicule: idVehicule) { failure in
+            self.result = failure
+        }
+        XCTAssertFalse(self.result)
+    }
+    
+    func test_Clicks_on_invoice() {
+        
+    }
+    
+    func test_Gets_information_of_the_invoice() {
+        
+    }
+}
+
+
+class InvoiceService {
+    private var httpCode: Int
+    
+    init(httpCode: Int) {
+        self.httpCode = httpCode
+    }
+    
+    func getInvoice(idVehicule: String, onEnd: @escaping (_: Bool) -> Void) {
+        switch(httpCode) {
+        case 200:
+            onEnd(true)
+            
+        default:
+            onEnd(false)
+        }
+    }
 }
