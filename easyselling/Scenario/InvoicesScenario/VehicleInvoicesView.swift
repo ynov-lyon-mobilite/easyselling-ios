5af5bb936e98ff03bfcb1bd54c7cfde22e0346e2
PierreGourgouillon <72550004+PierreGourgouillon@users.noreply.github.com>
2021-12-15 16:11:29 +0100

    feature/delete_invoice (#35)
    * Delete_invoice finish
    
    * add text in localizable
    
    * Resolve conv PR

--- easyselling/Scenario/InvoicesScenario/VehicleInvoicesView.swift
+++ easyselling/Scenario/InvoicesScenario/VehicleInvoicesView.swift
@@ -42,6 +42,13 @@ struct VehicleInvoicesView: View {
                             await viewModel.downloadInvoiceContent(of: invoice.file)
                         }
                     }
+                    .swipeActions(edge: .trailing) {
+                        Button(L10n.Invoice.deleteButton) {
+                            Task {
+                                await viewModel.deleteInvoice(idInvoice: invoice.id)
+                            }
+                        }.tint(Asset.onBoardingDotActive.swiftUIColor)
+                    }
                 }
             }
         }
--- easysellingTests/InvoicesScenario_Specs/VehicleInvoiceViewModel_Specs.swift
+++ easysellingTests/InvoicesScenario_Specs/VehicleInvoiceViewModel_Specs.swift
@@ -46,10 +46,29 @@ class VehicleInvoiceViewModel_Specs: XCTestCase {
         thenError(is: "Une erreur est survenue")
     }
 
+    func test_Deletes_invoice_when_request_is_success() async {
+        givenViewModel(vehicleInvoicesGetter: SucceedingVehicleInvoicesGetter(expectedVehicleInvoices),
+                              invoiceDeletor: SucceedingInvoiceDeletor())
+        await whenTryingToGetVehicleInvoices()
+        await whenDeletingInvoice(withId: 1)
+        thenLoadInvoices(are: [
+            Invoice(id: 2, vehicle: "1", file: "2", dateCreated: "date2", dateUpdated: ""),
+            Invoice(id: 3, vehicle: "2", file: "3", dateCreated: "date3", dateUpdated: "")])
+    }
+
+    func test_Shows_an_error_when_the_request_fails_when_deleting_an_invoice() async {
+        givenViewModel(vehicleInvoicesGetter: SucceedingVehicleInvoicesGetter(expectedVehicleInvoices), invoiceDeletor: FailingInvoiceDeletor(withError: APICallerError.notFound))
+        await whenDeletingInvoice(withId: 1)
+        thenError(is: "Impossible de trouver ce que vous cherchez")
+    }
+
     private func givenViewModel(vehicleInvoicesGetter: VehicleInvoicesGetter,
                                 invoiceFileInformationsGetter: InvoiceFileInformationsGetter = SucceedingFileInformationsGetter(),
-                                invoiceDownloader: InvoiceDownloader = SucceedingInvoiceDownloader()) {
+                                invoiceDownloader: InvoiceDownloader = SucceedingInvoiceDownloader(),
+                                invoiceDeletor: InvoiceDeletor = SucceedingInvoiceDeletor()) {
+
         viewModel = VehicleInvoiceViewModel(ofVehicleId: vehicleId,
+                                            invoiceDeletor: invoiceDeletor,
                                             vehicleInvoicesGetter: vehicleInvoicesGetter,
                                             invoiceDownloader: invoiceDownloader,
                                             invoiceFileInformationsGetter: invoiceFileInformationsGetter,
@@ -57,6 +76,17 @@ class VehicleInvoiceViewModel_Specs: XCTestCase {
             self.onNavigatingToInvoiceView = true
             self.downloadedInvoice = invoice
         })
+    }
+
+    private func givenViewModelDeletor(invoicesGetter: VehicleInvoicesGetter, invoiceDeletor: InvoiceDeletor) {
+        viewModel = VehicleInvoiceViewModel(ofVehicleId: vehicleId, invoiceDeletor: invoiceDeletor, vehicleInvoicesGetter: invoicesGetter,invoiceDownloader: SucceedingInvoiceDownloader(), invoiceFileInformationsGetter: SucceedingFileInformationsGetter(), onNavigatingToInvoiceView: { invoice in
+            self.onNavigatingToInvoiceView = true
+            self.downloadedInvoice = invoice
+        })
+    }
+
+    private func whenDeletingInvoice(withId: Int) async {
+        await viewModel.deleteInvoice(idInvoice: withId)
     }
 
     private func whenTryingToGetVehicleInvoices() async {
@@ -85,6 +115,10 @@ class VehicleInvoiceViewModel_Specs: XCTestCase {
 
     private func thenError(is expected: String) {
         XCTAssertEqual(expected, viewModel.error?.errorDescription)
+    }
+
+    private func thenLoadInvoices(are expected: [Invoice]) {
+        XCTAssertEqual(expected, viewModel.invoices)
     }
 
     private var viewModel: VehicleInvoiceViewModel!
