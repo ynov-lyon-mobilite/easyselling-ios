cccebaed2976eb81d270bffad9938c6cb83547be
Corentin Laurencine <corentin.laurencine@ynov.com>
2022-01-07 15:21:09 +0100

    Update MyVehiclesView.swift

--- easyselling/Scenario/VehiclesScenario/MyVehicles/MyVehiclesView.swift
+++ easyselling/Scenario/VehiclesScenario/MyVehicles/MyVehiclesView.swift
@@ -14,6 +14,7 @@ struct MyVehiclesView: View {
     var body: some View {
         NavigationView {
             VStack {
+                Button("Filtrer") {}
                 List {
                     if viewModel.state == .loading {
                         ProgressView()
