4740e72b221cfb8e8784fc05b89ac8c50d00e510
Corentin Laurencine <corentin.laurencine@ynov.com>
2022-03-09 16:44:15 +0100

    PR creation

---
+++ easyselling/Additions/UI/Components/Loader.swift
@@ -0,0 +1,20 @@
+//
+//  Loader.swift
+//  easyselling
+//
+//  Created by Corentin Laurencine on 09/03/2022.
+//
+
+import SwiftUI
+
+struct Loader: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Loader_Previews: PreviewProvider {
+    static var previews: some View {
+        Loader()
+    }
+}
