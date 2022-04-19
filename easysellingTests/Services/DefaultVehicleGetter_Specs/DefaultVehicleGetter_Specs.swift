806237fafdbfc521ca9e8d4240aff338fd4a2d58
Nicolas Barbosa <nicolas.barbosa@orange.com>
2022-04-19 20:40:37 +0200

    Refactor tests

--- easysellingTests/Services/DefaultVehicleGetter_Specs/DefaultVehicleGetter_Specs.swift
+++ easysellingTests/Services/DefaultVehicleGetter_Specs/DefaultVehicleGetter_Specs.swift
@@ -10,28 +10,38 @@ import XCTest
 
 class DefaultSharedVehicleGetter_Specs: XCTestCase {
     func test_Throws_error_when_request_failed() async {
-        let sharedVehiclesGetter = DefaultSharedVehiclesGetter(requestGenerator: FakeAuthorizedRequestGenerator(), apiCaller: FailingAPICaller(withError: 404))
-        do {
-            _ = try  await sharedVehiclesGetter.getSharedVehicles()
-        } catch(let error) {
-            self.error = (error as! APICallerError)
-        }
-        XCTAssertEqual(.notFound, self.error)
+        givenShareVehiclesGetter(apiCaller: FailingAPICaller(withError: 404))
+        await whenGettingSharedVehicles()
+        thenError(is: .notFound)
     }
 
     func test_Shows_vehicle_when_request_succeeded() async {
-        let expected = [Vehicle(id: "1", brand: "Peugeot", model: "model1", licence: "licence1", type: .car, year: "year1")]
+        givenShareVehiclesGetter()
+        await whenGettingSharedVehicles()
+        thenVehicles(are: [Vehicle(id: "1", brand: "Peugeot", model: "model1", licence: "licence1", type: .car, year: "year1")])
+    }
 
-        let vehicleGetter = DefaultSharedVehiclesGetter(requestGenerator: FakeAuthorizedRequestGenerator(), apiCaller: DefaultAPICaller(urlSession: FakeUrlSession(localFile: .succeededVehicle)))
+    private func givenShareVehiclesGetter(apiCaller: APICaller = DefaultAPICaller(urlSession: FakeUrlSession(localFile: .succeededVehicle))) {
+        vehicleGetter = DefaultSharedVehiclesGetter(requestGenerator: FakeAuthorizedRequestGenerator(), apiCaller: apiCaller)
+    }
+
+    private func whenGettingSharedVehicles() async {
         do {
             vehicles = try await vehicleGetter.getSharedVehicles()
         } catch(let error) {
             self.error = (error as! APICallerError)
         }
+    }
 
+    private func thenVehicles(are expected: [Vehicle]) {
         XCTAssertEqual(expected, vehicles)
     }
 
+    private func thenError(is expected: APICallerError) {
+        XCTAssertEqual(expected, self.error)
+    }
+
+    private var vehicleGetter: DefaultSharedVehiclesGetter!
     private var vehicles: [Vehicle]!
     private var error: APICallerError!
 }
