032d6ed0c69d2f55be764bc7bed7ee899dc12431
Pierre <Pierre_gourgouillon@hotmail.com>
2022-04-23 17:59:47 +0200

    add unit test LaunchScreenScenario

--- easyselling/Scenario/LaunchScreenScenario/LaunchScreenView.swift
+++ easyselling/Scenario/LaunchScreenScenario/LaunchScreenView.swift
@@ -9,8 +9,29 @@ import SwiftUI
 
 struct LaunchScreenView: View {
     let onFinish: Action
+    @State var width: CGFloat = 300
+    let timer: Double = 2.0
+    @State var isFinish = false
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .center) {
+            Image("logo")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: width, height: width)
+                .clipped()
+        }
+        .onAppear {
+            startLaunchScreen()
+        }
+    }
+
+    func startLaunchScreen() {
+        withAnimation(.easeInOut(duration: timer)) {
+            width = 400
+        }
+        Timer.scheduledTimer(withTimeInterval: timer, repeats: false, block: { _ in
+            self.onFinish()
+        })
     }
 }
 
